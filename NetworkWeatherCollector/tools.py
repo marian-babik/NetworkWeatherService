3115ec7eff52db67157993a952448334b3cc16a4
Ilija Vukotic <ivukotic@uchicago.edu>
2017-04-18 19:11:42 +0200

    refactoring conted...

--- NetworkWeatherCollector/tools.py
+++ NetworkWeatherCollector/tools.py
@@ -16,3 +16,25 @@ def GetESConnection():
     time.sleep(70)
     GetESConnection()
 
+
+def bulk_index(data , es=None, thread_name=''):
+	reconnect = True
+    if es is None:
+        es = GetESConnection()
+    try:
+        res = helpers.bulk(es, data, raise_on_exception=True, request_timeout=60)
+        print(thread_name, "inserted:", res[0], 'errors:', res[1])
+        data = []
+        reconnect = False
+    except es_exceptions.ConnectionError as e:
+        print('ConnectionError ', e)
+    except es_exceptions.TransportError as e:
+        print('TransportError ', e)
+    except helpers.BulkIndexError as e:
+        print(e[0])
+      # for i in e[1]:
+      # print(i)
+    except:
+        print('Something seriously wrong happened.')
+    if reconnect:
+        es = GetESConnection()
